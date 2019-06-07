29dbf2aba2a652d900712d2e508f8f7d65f56440
AliAbdulle <alidhobey@gmail.com>
2019-06-07 15:16:37 -0500

    Saved before the class

--- Program.cs
+++ Program.cs
@@ -56,7 +56,7 @@ namespace linq
 
             List<int> ascending = numbersList.OrderBy(n => n).ToList();
 
-                Console.WriteLine($"The collection of the numbers sorted by ascending is:");
+            Console.WriteLine($"The collection of the numbers sorted by ascending is:");
             foreach (int number in ascending)
             {
                 Console.WriteLine(number);
@@ -95,6 +95,72 @@ namespace linq
             */
             List<int> wheresSquaredo = new List<int>()
             {66, 12, 8, 27, 82, 34, 7, 50, 19, 46, 81, 23, 30, 4, 68, 14};
+
+            bool IsNumberSquare(int num)
+            {
+                int number = (int)Math.Sqrt(num);
+                return num == number * number;
+            }
+            IEnumerable<int> squareNumbers = wheresSquaredo.TakeWhile(num => !IsNumberSquare(num));
+
+            Console.WriteLine($"The perfect squere List detected is ");
+            foreach (int num in squareNumbers)
+            {
+                Console.WriteLine(num);
+            }
+            Console.WriteLine("------------------------------------------------------------");
+
+            // Build a collection of customers who are millionaires
+public class Customer
+        {
+            public string Name { get; set; }
+            public double Balance { get; set; }
+            public string Bank { get; set; }
+        }
+
+        public static void Main()
+        {
+            List<Customer> customers = new List<Customer>() {
+            new Customer(){ Name="Bob Lesman", Balance=80345.66, Bank="FTB"},
+            new Customer(){ Name="Joe Landy", Balance=9284756.21, Bank="WF"},
+            new Customer(){ Name="Meg Ford", Balance=487233.01, Bank="BOA"},
+            new Customer(){ Name="Peg Vale", Balance=7001449.92, Bank="BOA"},
+            new Customer(){ Name="Mike Johnson", Balance=790872.12, Bank="WF"},
+            new Customer(){ Name="Les Paul", Balance=8374892.54, Bank="WF"},
+            new Customer(){ Name="Sid Crosby", Balance=957436.39, Bank="FTB"},
+            new Customer(){ Name="Sarah Ng", Balance=56562389.85, Bank="FTB"},
+            new Customer(){ Name="Tina Fey", Balance=1000000.00, Bank="CITI"},
+            new Customer(){ Name="Sid Brown", Balance=49582.68, Bank="CITI"}
+
+        };
+            public string customer(string name, int balance, string bank )
+            {
+                if (balance > 1000000)
+                {
+                    Console.WriteLine(bank)
+                }else
+                {
+                    Console.WriteLine("Not found");
+                }
+            }
+        /*
+            Given the same customer set, display how many millionaires per bank.
+            Ref: https://stackoverflow.com/questions/7325278/group-by-in-linq
+
+            Example Output: 
+            WF 2
+            BOA 1
+            FTB 1
+            CITI 1
+        */
+
+
+
+
+
+
         }
     }
 }
+
+
